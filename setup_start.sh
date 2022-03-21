444b4821a44debef64373ab3b1cd29254c669453
typeRob <roberto@fabietti.it>
2022-03-21 14:52:43 +0100

    edit for db password

--- setup_start.sh
+++ setup_start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "insert absolute path of the home folder:"
 read dev_path
-echo "path inserted: " $path
-if [[ $dev_path ]] ; then
-    sed -i "s+=.*+="$dev_path"+g" .env
-    docker compose up -d
-else
-    echo "error, try again"
-fi
+echo "path inserted: " $dev_path
+echo "insert database root password:"
+read db_pw
+echo "password inserted: " $db_pw
+sed -i "s+DEV_PATH=.*+DEV_PATH="$dev_path"+g" .env
+sed -i "s+DB_ROOT_PASSWORD=.*+DB_ROOT_PASSWORD="$db_pw"+g" .env
+docker compose up -d

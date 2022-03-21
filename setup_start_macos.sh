ff87fa1b0c3b7f1f3ab4c3e45a2745dd38e8165c
typeRob <roberto@fabietti.it>
2022-03-21 14:52:59 +0100

    edit for db root password

--- setup_start_macos.sh
+++ setup_start_macos.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 echo "insert absolute path of the home folder:"
 read dev_path
-echo "path inserted: " $path
-if [[ $dev_path ]] ; then
-    sed -i '.bak' "s+=.*+="$dev_path"+g" .env
-    docker compose up -d
-else
-    echo "error, try again"
-fi
+echo "path inserted: " $dev_path
+echo "insert database root password:"
+read db_pw
+echo "password inserted: " $db_pw
+sed -i '.bak' "s+DEV_PATH=.*+DEV_PATH="$dev_path"+g" .env
+sed -i '.bak' "s+DB_ROOT_PASSWORD=.*+DB_ROOT_PASSWORD="$db_pw"+g" .env
+docker compose up -d

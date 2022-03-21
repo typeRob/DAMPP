37ae159e3877c3b4f9141fb6cb1f4b6262e81985
typeRob <roberto@fabietti.it>
2022-03-21 13:49:20 +0100

    edited to work on linux distros

--- setup_start.sh
+++ setup_start.sh
@@ -3,7 +3,7 @@ echo "insert absolute path of the home folder:"
 read dev_path
 echo "path inserted: " $path
 if [[ $dev_path ]] ; then
-    sed -i '.bak' "s+=.*+="$dev_path"+g" .env
+    sed -i "s+=.*+="$dev_path"+g" .env
     docker compose up -d
 else
     echo "error, try again"

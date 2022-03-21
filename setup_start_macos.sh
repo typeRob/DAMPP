3849f24e783503d8497582393b611197f3e7386f
typeRob <roberto@fabietti.it>
2022-03-21 16:18:51 +0100

    removed because it's not useful anymore

--- setup_start_macos.sh
+++
@@ -1,9 +0,0 @@
-echo "insert absolute path of the home folder:"
-read dev_path
-echo "path inserted: " $dev_path
-echo "insert database root password:"
-read db_pw
-echo "password inserted: " $db_pw
-sed -i '.bak' "s+DEV_PATH=.*+DEV_PATH="$dev_path"+g" .env
-sed -i '.bak' "s+DB_ROOT_PASSWORD=.*+DB_ROOT_PASSWORD="$db_pw"+g" .env
-docker compose up -d

ca5d57442d968aa4b6eebc8f03ca7c4f3954355d
typeRob <roberto@fabietti.it>
2022-03-21 16:21:53 +0100

    edited for the usage of a docker volume

--- setup_start.sh
+++ setup_start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo "insert absolute path of the home folder:"
+echo "insert absolute path of the home folder(~ doesn't work):"
 read dev_path
 echo "path inserted: " $dev_path
 echo "insert database root password:"
 read db_pw
 echo "password inserted: " $db_pw
-sed -i "s+DEV_PATH=.*+DEV_PATH="$dev_path"+g" .env
-sed -i "s+DB_ROOT_PASSWORD=.*+DB_ROOT_PASSWORD="$db_pw"+g" .env
+docker volume create --driver local \
+--opt type=none \
+--opt device=$dev_path \
+--opt o=bind dev
+sed -i '.bak '"s+DB_ROOT_PASSWORD=.*+DB_ROOT_PASSWORD="$db_pw"+g" .env
 docker compose up -d
